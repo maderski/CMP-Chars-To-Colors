5096dea693a6ac381e879a11a8eb7fd67c7427dc
Jason Maderski <maderski@gmail.com>
2025-04-14 12:49:51 -0400

    Fix Android staus bars not shown when changing theme and set background for screen to white

--- composeApp/src/androidMain/kotlin/org/maderskitech/cmpcharstocolors/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/org/maderskitech/cmpcharstocolors/MainActivity.kt
@@ -1,22 +1,64 @@
 package org.maderskitech.cmpcharstocolors
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.tooling.preview.Preview
-import org.maderskitech.cmpcharstocolors.platform.KeyboardControl
+import androidx.core.view.WindowInsetsControllerCompat
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.compose.LocalLifecycleOwner
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        enableEdgeToEdge()
         setContent {
+            var isDark = isSystemInDarkTheme()
+            var backgroundColor = if (isDark) Color.Black else Color.White
+            UpdateStatusBarForTheme(this@MainActivity, isDark)
+            Box(modifier = Modifier.fillMaxSize().background(backgroundColor))
             App()
         }
     }
 }
+@Composable
+fun UpdateStatusBarForTheme(activity: Activity, isDarkTheme: Boolean) {
+    val view = LocalView.current
+    val window = (view.context as Activity).window
+    val lifecycleOwner = LocalLifecycleOwner.current
+    val useLightIcons = !isDarkTheme
+
+    DisposableEffect(lifecycleOwner, isDarkTheme) {
+        val insetsController = WindowInsetsControllerCompat(window, view)
+
+        val observer = LifecycleEventObserver { _, event ->
+            if (event == Lifecycle.Event.ON_RESUME) {
+                insetsController.isAppearanceLightStatusBars = useLightIcons
+            }
+        }
+
+        insetsController.isAppearanceLightStatusBars = useLightIcons
+
+        lifecycleOwner.lifecycle.addObserver(observer)
+
+        onDispose {
+            lifecycleOwner.lifecycle.removeObserver(observer)
+        }
+    }
+}
+
 
 @Preview
 @Composable
--- composeApp/src/commonMain/kotlin/org/maderskitech/cmpcharstocolors/App.kt
+++ composeApp/src/commonMain/kotlin/org/maderskitech/cmpcharstocolors/App.kt
@@ -1,9 +1,11 @@
 package org.maderskitech.cmpcharstocolors
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import org.maderskitech.cmpcharstocolors.colorgrid.presentation.ColorGridScreen
 
@@ -14,6 +16,7 @@ fun App() {
         ColorGridScreen(
             modifier = Modifier
                 .safeDrawingPadding()
+                .background(Color.White)
         )
     }
 }
