b255e7f72b38f6e3b2d49f1858ceb684913d56af
Jason Maderski <maderski@gmail.com>
2025-04-13 20:32:51 -0400

    remove padding for now

--- composeApp/src/commonMain/kotlin/org/maderskitech/cmpcharstocolors/colorgrid/presentation/ColorGridViewModel.kt
+++ composeApp/src/commonMain/kotlin/org/maderskitech/cmpcharstocolors/colorgrid/presentation/ColorGridViewModel.kt
@@ -10,23 +10,6 @@ class ColorGridViewModel(
 
     fun getColorList(message: String, columnCount: Int): List<Color> {
         val colorIntList = stringToARGBListConverter.convert(message)
-        val paddedColorInts = padColorIntsToGrid(colorIntList, columnCount)
-        return paddedColorInts.map { Color(it) }
-    }
-
-    /*
-     The colors may not fill out a grid with a specified column count and if not, fill
-     the remaining by taking colors from the beginning of the list and thus starting to
-     repeat until the grid is filled.
-     */
-    private fun padColorIntsToGrid(colorIntList: List<Int>, columnCount: Int): List<Int> {
-        val remainder = colorIntList.size % columnCount
-        return if (remainder == 0) {
-            colorIntList
-        } else {
-            val needed = columnCount - remainder
-            val repeatColorInts = colorIntList.take(needed)
-            colorIntList + repeatColorInts
-        }
+        return colorIntList.map { Color(it) }
     }
 }
